2d2babef746aae368ca626f522d345bc4a2fbb32
Rohan Mukherjee <mrohan700@gmail.com>
2026-03-16 08:48:19 +0530

    Configured API routes in core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from rest_framework.routers import DefaultRouter
+from events.views import EventViewSet
+
+router = DefaultRouter()
+
+router.register('events', EventViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
