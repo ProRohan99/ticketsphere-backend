1973a9845fe7068af0d7a8d8968d581ee9615480
Rohan Mukherjee <mrohan700@gmail.com>
2026-03-16 10:08:42 +0530

    For event filtering API again configured view.py and urls.py inside events

--- events/urls.py
+++ events/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import EventViewSet
+from .views import EventViewSet, EventListAPI
 
 router = DefaultRouter()
 router.register(r'', EventViewSet, basename='events')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', EventListAPI.as_view(), name='event-list'),
 ]
--- events/views.py
+++ events/views.py
@@ -3,6 +3,19 @@ from .models import Event
 from .serializers import EventSerializer
 
 # Create your views here.
-class EventViewSet(ModelViewSet):
-    queryset = Event.objects.all()
+class EventListAPI(ListAPIView):
     serializer_class = EventSerializer
+
+    def get_queryset(self):
+        queryset = Event.objects.all()
+
+        category = self.request.query_params.get('category')
+        location = self.request.query_params.get('location')
+
+        if category:
+            queryset = queryset.filter(category=category)
+
+        if location:
+            queryset = queryset.filter(location=location)
+
+        return queryset
